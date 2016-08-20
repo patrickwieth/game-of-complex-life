921e2abdb6a3b1fe5673f55d655b9dbfd413e635
robrobo <robin.horstmann@gmx.net>
2016-08-20 21:16:20 +0200

    mal schauen

--- modelpy/example.py
+++ modelpy/example.py
@@ -2,53 +2,34 @@ import gameOfComplexLife as gocl
 import cellularAutomaton as ca
 import numpy as np
 
-#Initialisieren
+# Initialisieren
 mooreNeighborhood = ca.Neighborhood(gocl.initNeighborhood)
 gameOfLife = ca.CellularAutomaton(mooreNeighborhood)
 
 init = gameOfLife.init
 evolve = gameOfLife.evolve
 
-def printAllStates():
-    def printState(cell):
-        print(cell.state)  # console.log
-    gameOfLife.applyFunc(printState)
 
-def registerDecisions(species, decisions):
-    gameOfLife.decisions[species] = decisions
+def makeDecision(cell):
+    if cell.state['species'] == 'OnlyClone':
+        return {'action': "clone", 'value': int(np.random.ranf() * 6)}
 
-def getState():
-    return gameOfLife.world['space']
+    if cell.state['species'] == 'CloneAndAttack':
+        freeNeighbors = []
+        for i in range(len(cell.neighbors)):
+            if cell.neighbors[i].state['species'] != 'empty':
+                if cell.neighbors[i].state['color'] != cell.state['color']:
+                    return {'action': 'fight', 'value': i}
+            else:
+                freeNeighbors.append(i)
 
-def killAll(species):
-    def func(cell):
-        if cell.state.species == species:
-            cell.kill()
-    gameOfLife.applyFunc(func)
+        if (len(freeNeighbors) > 0):
+            return {'action': "clone", 'value': freeNeighbors[int(np.random.ranf() * len(freeNeighbors))]}
+        else:
+            return {'action': "stay", 'value': int(np.random.ranf() * 6)}
 
-def makeDecision():
-    return {'action': "clone", 'value': int(np.random.ranf()*6)}
-    freeNeighbors = []
-    """
-    for(i = 0 i < len(this.neighbors) i++) {
-        if(this.neighbors[i].state.species !== 'empty') {
-            if(this.neighbors[i].state.color !== this.color) {
-                return {
-                    action: "fight",
-                    value: i
-        else {
-            freeNeighbors.push(i)
+    return {'action': "clone", 'value': int(np.random.ranf() * 6)}
 
-    if(len(freeNeighbors) > 0) {
-        return {
-            action: "clone",
-            value: freeNeighbors[Math.floor(Math.random()*len(freeNeighbors))]
-
-    else {
-        return {
-            action: "stay",
-            value: Math.floor(Math.random() * 6)
-"""
 
 def newSpecies(clientId, event):
     gameOfLife.decisions[event['species']] = []
@@ -59,6 +40,58 @@ def newSpecies(clientId, event):
     newCell = ca.Cell(func)
     gameOfLife.world['space'][event['position']['x']][event['position']['y']].state = newCell.state
     gameOfLife.world['space'][event['position']['x']][event['position']['y']].futureState = newCell.futureState
+
+
+# ===================HIER BEGINNT DAS BEISPIEL================
+
+gameOfLife.parameters = gameOfLife.world['parameters']
+newSpecies(np.random.randint(0, 10), {'species': 'OnlyClone', 'color': 'Blue', 'position': {'x': 0, 'y': 0}})
+newSpecies(np.random.randint(0, 10), {'species': 'OnlyClone', 'color': 'Blue', 'position': {'x': 0, 'y': 1}})
+newSpecies(np.random.randint(0, 10), {'species': 'CloneAndAttack', 'color': 'Green', 'position': {'x': 2, 'y': 2}})
+
+for _ in range(20):
+    dec = {}
+    for j, n in enumerate(gameOfLife.world["space"]):
+        for i, m in enumerate(n):
+            print(
+                '{:2d} {:2d} {:15s} {:5s} {:8s} {:2d}'.format(j, i, m.state['species'], m.state['color'], m.goal['action'], m.state['energy']))
+            if m.state['species'] != 'empty':
+                try:
+                    dec[m.state['species']].append(makeDecision(m))
+                except:
+                    dec[m.state['species']] = [makeDecision(m)]
+    print(dec)
+    gameOfLife.decisions = dec
+    evolve()
+for j, n in enumerate(gameOfLife.world["space"]):
+    for i, m in enumerate(n):
+        print(
+                '{:2d} {:2d} {:15s} {:5s} {:8s} {:2d}'.format(j, i, m.state['species'], m.state['color'], m.goal['action'], m.state['energy']))
+
+
+# ===================Unbenutztes ist hier unten ========================
+
+def printAllStates():
+    def printState(cell):
+        print(cell.state)  # console.log
+
+    gameOfLife.applyFunc(printState)
+
+
+def registerDecisions(species, decisions):
+    gameOfLife.decisions[species] = decisions
+
+
+def getState():
+    return gameOfLife.world['space']
+
+
+def killAll(species):
+    def func(cell):
+        if cell.state.species == species:
+            cell.kill()
+
+    gameOfLife.applyFunc(func)
 
 
 def setParameters(event):
@@ -90,22 +123,3 @@ def shuffle(array):
         array[randomIndex] = temporaryValue
 
     return array
-
-#===================HIER BEGINNT DAS BEISPIEL================
-
-gameOfLife.parameters = gameOfLife.world['parameters']
-newSpecies(np.random.randint(0,10),{'species': 'test', 'color': 'Red', 'position':{'x': 0, 'y':0}})
-
-for _ in range(3):
-    dec = []
-    for j,n in enumerate(gameOfLife.world["space"]):
-        for i,m in enumerate(n):
-            print(j,i,m.state['species'],m.goal,m.state)
-            if m.state['species'] == 'test': dec.append(makeDecision())
-    print(dec)
-    registerDecisions('test', dec)
-    evolve()
-for j,n in enumerate(gameOfLife.world["space"]):
-    for i,m in enumerate(n):
-        print(j,i,m.state['species'],m.goal,m.state)
-
---
+++ modelpy/gameOfComplexLife.py
@@ -0,0 +1,57 @@
+import cellularAutomaton as ca
+import numpy as np
+
+CORPSE_COLOR = "black"
+EMPTY_COLOR = "white"
+
+
+def initEmpty():
+    return {'color': EMPTY_COLOR, 'species': 'empty', 'energy': 0}
+
+
+def initNeighborhood():
+    world = {}
+    space = []
+    size = {'x': 3, 'y': 3}
+
+    for i in range(size['x']):
+        space.append([])
+        for j in range(size['y']):
+            space[i].append(ca.Cell(initEmpty))
+
+    # the good modulo (works for negative values also)
+    def mod(n, m):
+        return ((n % m) + m) % m
+
+    # hexagonal neighboorhood
+    for i in range(size['x']):
+        for j in range(size['y']):
+            # left and right neighbors
+            space[i][j].neighbors.append(space[mod(i + 1, size['x'])][j])
+            space[i][j].neighbors.append(space[mod(i - 1, size['x'])][j])
+
+            # upper neighbors
+            space[i][j].neighbors.append(space[mod(i + j % 2 - 1, size['x'])][mod(j + 1, size['y'])])
+            space[i][j].neighbors.append(space[mod(i + j % 2, size['x'])][mod(j + 1, size['y'])])
+
+            # lower neighbors
+            space[i][j].neighbors.append(space[mod(i + j % 2 - 1, size['x'])][mod(j - 1, size['y'])])
+            space[i][j].neighbors.append(space[mod(i + j % 2, size['x'])][mod(j - 1, size['y'])])
+
+    world['space'] = space
+
+    # this is not in use, but should be!
+    world['parameters'] = {
+
+        'energy': {
+            'stay': 1,
+            'move': 2,
+            'fight': 4,
+            'clone': 10,
+            'wall': 1,
+            'fromEmptyCells': 1,
+            'fromSun': 0
+        }
+    }
+
+    return world
